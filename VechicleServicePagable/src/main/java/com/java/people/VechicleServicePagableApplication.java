ce47278df8b6e2684d269b8a5b5d4ce83598a2c5
lenovo <lenovo@DESKTOP-31C3901>
2018-06-20 10:53:22 +0530

    Done By Hari

---
+++ VechicleServicePagable/src/main/java/com/java/people/VechicleServicePagableApplication.java
@@ -0,0 +1,12 @@
+package com.java.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VechicleServicePagableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VechicleServicePagableApplication.class, args);
+	}
+}
